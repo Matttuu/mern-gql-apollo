7ec9665ca28ab3d4cf7ccfa15e7e7471859368e1
Mathias Blomgaard <xuniqh@gmail.com>
2019-09-05 11:25:42 +0200

    Added graphqlSchema

---
+++ server/config/modules/post/graphqlSchema.js
@@ -0,0 +1,22 @@
+// #1 Import the gql method from apollo-server-express
+const { gql } = require('apollo-server-express');
+
+// #2 Construct a schema with gql and using the GraphQL schema language
+const typeDefs = gql` 
+// #3 Define the respective type with three fields
+// #Note that the _id is created automatically by mongoose
+    type Post {
+        _id: ID,
+        title: String,
+        content: String
+    },
+    #4 Define the query type that must respond to 'posts' query
+    type Query{
+        posts:[post]
+    },
+    #5 Define a mutation to add new posts with two qreuired fields
+    type Mutation {
+        addpost(title: String!, content: String!): Post,
+    }`;
+
+module.exports = typeDefs;
